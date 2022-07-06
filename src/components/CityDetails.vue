b3fe95bd3b97cff669648e7522e2a91a9c6b9490
mamasza <mamamagdac@gmail.com>
2022-07-06 19:01:50 +0200

    2 files added

---
+++ src/components/CityDetails.vue
@@ -0,0 +1,37 @@
+template>
+  <div v-if="Object.keys(values).length == 0" class="loading">loading...</div>
+  <div v-else>
+    <h2>{{ values.name }}</h2>
+    <img
+      :src="'https://countryflagsapi.com/svg/' + values.country"
+      alt="COUNTRY FLAG"
+    />
+    <div class="suntime">
+      <div class="sunrise">{{ unixToDateTime(values.sunrise) }}</div>
+      <div class="sunset">{{ unixToDateTime(values.sunset) }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    values: Object,
+  },
+  methods: {
+    unixToDateTime(unix_timestamp) {
+      const date = new Date(unix_timestamp * 1000);
+      const hours = date.getHours();
+      const minutes = '0' + date.getMinutes();
+      const seconds = '0' + date.getSeconds();
+      return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    },
+  },
+};
+</script>
+
+<style>
+img {
+  max-width: 30vw;
+}
+</style>
---
+++ src/components/SingleWeather.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="single">
+    <h3>{{ values.dt_txt }}</h3>
+    <div class="temp">
+      <ul>
+        <li>temp: {{ values.main.temp }} &deg; C</li>
+        <li>min: {{ values.main.temp_min }} &deg; C</li>
+        <li>max: {{ values.main.temp_max }} &deg; C</li>
+        <li>ciśnienie : {{ values.main.pressure }} hPa</li>
+      </ul>
+      <img
+        :src="
+          'http://openweathermap.org/img/wn/' +
+          values.weather[0].icon +
+          '@2x.png'
+        "
+        alt="Weather img"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    values: Object,
+  },
+};
+</script>
+
+<style>
+.single {
+  border: 2px dashed red;
+}
+</style>
